e74dfc0dfed3db92f6e15f283d7f6b48fa27651e
Clément Haller <clement.haller95@hotmail.fr>
2019-06-13 18:44:29 +0200

    fix bug

--- src/controller/PlayerStatController.js
+++ src/controller/PlayerStatController.js
@@ -68,7 +68,7 @@ module.exports = {
                 playerStatModel.selectAllPointOfCareer(playerStat.player_name)
                     .then((nbPointCarrier) => {
                         console.log(nbPointCarrier);
-                        let seassonTab = ["2010-11", "2011-12", "2012-13", "2013-14", "2014-15", "2016-17", "2017-18", "2018-19"];
+                        let seassonTab = ["2010-11", "2011-12", "2012-13", "2013-14", "2014-15", "2015,16" ,"2016-17", "2017-18", "2018-19"];
                         let newTab = [];
                         seassonTab.forEach((item) => {
                             newTab.push({
